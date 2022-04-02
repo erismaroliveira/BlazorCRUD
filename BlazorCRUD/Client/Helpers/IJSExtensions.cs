c2f6ee310d70ac69e0d6bde2b8a4bb7a4b4d328f
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-04-02 20:49:13 -0300

    Exportando dados CSV e Excel

---
+++ BlazorCRUD/Client/Helpers/IJSExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Client.Helpers
+{
+    public static class IJSExtensions
+    {
+        public static async Task<object> SalvarComo(this IJSRuntime js, string nomeArquivo, byte[] arquivo)
+        {
+            return await js.InvokeAsync<object>("saveAsFile", 
+                nomeArquivo, 
+                Convert.ToBase64String(arquivo));
+        }
+    }
+}
