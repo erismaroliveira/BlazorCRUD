93db7f6c23ed22f28353848703f631a79d4b78bc
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-04-03 13:08:45 -0300

    Utilizando sweetalert2 para mostrar alertas

--- BlazorCRUD/Client/Helpers/IJSExtensions.cs
+++ BlazorCRUD/Client/Helpers/IJSExtensions.cs
@@ -12,5 +12,29 @@ namespace BlazorCRUD.Client.Helpers
                 nomeArquivo, 
                 Convert.ToBase64String(arquivo));
         }
+
+        public static async Task<object> MostrarMensagem(this IJSRuntime js, string mensagem)
+        {
+            return await js.InvokeAsync<object>("Swal.fire", mensagem);
+        }
+
+        public static async Task<object> MostrarMensagem(this IJSRuntime js, string titulo, string mensagem, TipoMensagemSweetAlert tipoMensagemSweetAlert)
+        {
+            return await js.InvokeAsync<object>("Swal.fire", titulo, mensagem, tipoMensagemSweetAlert.ToString());
+        }
+
+        public static async Task<bool> Confirm(this IJSRuntime js, string titulo, string mensagem, TipoMensagemSweetAlert tipoMensagemSweetAlert)
+        {
+            return await js.InvokeAsync<bool>("customConfirm", titulo, mensagem, tipoMensagemSweetAlert.ToString());
+        }
+    }
+
+    public enum TipoMensagemSweetAlert
+    {
+        question,
+        warning,
+        error,
+        success,
+        info
     }
 }
--- BlazorCRUD/Client/wwwroot/js/Utilidades.js
+++ BlazorCRUD/Client/wwwroot/js/Utilidades.js
@@ -6,3 +6,23 @@
     link.click();
     document.body.removeChild(link);
 }
+
+function customConfirm(titulo, mensagem, tipo) {
+    return new Promise((resolve) => {
+        Swal.fire({
+            title: titulo,
+            text: mensagem,
+            type: tipo,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmar'
+        }).then((result) => {
+            if (result.value) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+}
